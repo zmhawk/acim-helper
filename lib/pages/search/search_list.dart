0f32f14c839462c3b136a10aa34e21453e79451c
Jianbo.Yu <2515100339@qq.com>
2023-12-26 19:59:24 +0800

    feat: 搜索页面跳转到首页

--- lib/pages/search/search_list.dart
+++ lib/pages/search/search_list.dart
@@ -1,4 +1,5 @@
 import 'package:acim_helper/models/data.dart';
+import 'package:acim_helper/models/text.dart';
 import 'package:acim_helper/pages/search/search_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -15,6 +16,8 @@ class SearchResultList extends HookWidget {
   Widget build(BuildContext context) {
     List<DataItem> dataList = context.watch<DataModel>().data;
 
+    CurrentText currentText = useContext().watch<CurrentText>();
+
     print('search result list build: $keyword, $tab');
 
     SearchResult result = SearchResult(data: [], length: 0, page: 1, total: 0);
@@ -29,10 +32,19 @@ class SearchResultList extends HookWidget {
     return ListView.separated(
         itemBuilder: (context, index) {
           return ListTile(
-              title: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Text(result.data[index].text),
-          ));
+            title: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Text(result.data[index].text),
+            ),
+            onTap: () {
+              currentText.changeText(result.data[index].index);
+              Navigator.pushNamedAndRemoveUntil(
+                context,
+                Navigator.defaultRouteName,
+                (route) => false,
+              );
+            },
+          );
         },
         separatorBuilder: (context, index) {
           return const Divider();

fccdecc90906e33d8ea389b514c979f5a049581e
Jianbo.Yu <2515100339@qq.com>
2023-12-27 11:14:50 +0800

    feat: 联系作者

--- lib/pages/menu/menu.dart
+++ lib/pages/menu/menu.dart
@@ -1,6 +1,7 @@
 import 'package:acim_helper/pages/history/history.dart';
 import 'package:acim_helper/pages/settings/settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class MenuDrawer extends StatefulWidget {
   const MenuDrawer({Key? key}) : super(key: key);
@@ -42,6 +43,29 @@ class _MenuDrawerState extends State<MenuDrawer> {
           onTap: () {
             Navigator.push(context,
                 MaterialPageRoute(builder: (context) => const SettingPage()));
+          },
+        ),
+        ListTile(
+          title: const Text('联系作者'),
+          onTap: () async {
+            Clipboard.setData(const ClipboardData(text: '逆行者的奇迹漫谈'));
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('联系作者'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text('微信公众号：逆行者的奇迹漫谈'),
+                        Text(
+                          '（已复制到剪贴板）',
+                          style: themeData.textTheme.bodySmall,
+                        )
+                      ],
+                    ),
+                  );
+                });
           },
         ),
       ],
